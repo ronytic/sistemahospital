41c91cf2b95cc1807d75d665766b559e28231cab
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-06-13 19:47:30 -0400

    Inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema Hospital
                 </div>
 
                 <div class="links">
